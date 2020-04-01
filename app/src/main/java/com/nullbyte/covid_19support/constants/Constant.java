9c5572e4455544e3625a634b5de04e3c4300a71b
Harishiv Singh <harishiv8@gmail.com>
2020-04-01 21:33:05 +0530

    Update Constant.java

--- app/src/main/java/com/nullbyte/covid_19support/constants/Constant.java
+++ app/src/main/java/com/nullbyte/covid_19support/constants/Constant.java
@@ -4,6 +4,6 @@ public class Constant {
     public static final String HEADER_HOST_NAME = "x-rapidapi-host";
     public static final String HEADER_HOST_VALUE = "coronavirus-monitor.p.rapidapi.com";
     public static final String HEADER_KEY_NAME = "x-rapidapi-key";
-    public static final String HEADER_KEY_VALUE = "e62053511cmshf9ef1c3940f1009p184555jsn64ec6dcb7692";
+    public static final String HEADER_KEY_VALUE = "your_api_key";
     public static final String BASE_URL = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/";
 }
